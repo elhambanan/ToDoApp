298fc6cabb1387f7a667b3de5fadd95ef2067991
elhambanan <elham.banagozar@gmail.com>
2022-02-02 15:55:33 +0330

    modify modals

--- todoApp.js
+++ todoApp.js
@@ -1,12 +1,29 @@
 const showNavModal = document.querySelector(".navbar-modalBtn");
 const navbarModal = document.querySelector(".nav__modal");
+const todoMain = document.querySelector(".todo__main");
+const todoAddBtn = document.querySelector(".todo__add"); 
+const todoModal = document.querySelector(".todo__modal");
+const backDrop = document.querySelector(".backdrop");
+const todoModalClose = document.querySelector(".todo__nav");
 
 showNavModal.addEventListener("click", showModal);
-// closeBtn.addEventListener("click", closeModal);
+todoMain.addEventListener("click", closeModal);
 
-function showModal(params) {
+todoAddBtn.addEventListener("click", () => {
+    todoModal.style.opacity = 1;
+    backDrop.style.display = "block"
+})
+todoModalClose.addEventListener("click", ()=>{
+    todoModal.style.opacity = 0;
+    backDrop.style.display = "none";
+    console.log("close todo new")
+})
+
+function showModal() {
     navbarModal.classList.add("modal__appear"); 
 }
 function closeModal(){
-    modalBox.classList.remove("modal__appear");
+    navbarModal.classList.remove("modal__appear");
+    
 }
+
